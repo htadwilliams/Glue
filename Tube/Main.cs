5343d11832e10f0c5d0631e4e6d28d8e3efa341a
Oedwak <Oedwak@comcast.net>
2019-01-19 20:52:48 -0800

    Add app settings and persist main form checkbox states

--- Tube/Main.cs
+++ Tube/Main.cs
@@ -5,7 +5,6 @@ namespace Glue
 {
     public partial class Main : Form
     {
-
         public bool LogInput { get => logInput; set { logInput = value; IDC_RAWKEYNAMES.Enabled = value; } }
         public bool RawKeyNames { get => rawKeyNames; set => rawKeyNames = value; }
 
@@ -16,8 +15,13 @@ namespace Glue
         public Main()
         {
             InitializeComponent();
+
+            this.logInput = Properties.Settings.Default.LogInput;
+            this.rawKeyNames = Properties.Settings.Default.RawKeyNames;
+
             this.IDC_LOGDISPLAY.DataBindings.Add("Checked", this, "logInput", true, DataSourceUpdateMode.OnPropertyChanged);
             this.IDC_RAWKEYNAMES.DataBindings.Add("Checked", this, "rawKeyNames", true, DataSourceUpdateMode.OnPropertyChanged);
+
             IDC_RAWKEYNAMES.Enabled = logInput;
         }
 
@@ -29,6 +33,17 @@ namespace Glue
         private void IDB_CLEAR_Click(object sender, EventArgs e)
         {
             IDE_INPUTSTREAM.Clear();
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            Properties.Settings.Default.LogInput = this.logInput;
+            Properties.Settings.Default.RawKeyNames = this.RawKeyNames;
+
+            LOGGER.Info("Saving settings (Properties.Settings.Default.Save())");
+            Properties.Settings.Default.Save();
+
+            base.OnFormClosed(e);
         }
     }
 }
