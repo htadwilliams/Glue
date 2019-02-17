b002a1a66ca72771f660878be34c76afa0831117
Oedwak <Oedwak@comcast.net>
2019-02-17 15:23:50 -0800

    Add JsonConverter factory for derived types

--- Tube/Action.cs
+++ Tube/Action.cs
@@ -1,15 +1,83 @@
 ﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace Glue
 {
+    public class BaseSpecifiedConcreteClassConverter : DefaultContractResolver
+    {
+        protected override JsonConverter ResolveContractConverter(Type objectType)
+        {
+            if (typeof(Action).IsAssignableFrom(objectType) && !objectType.IsAbstract)
+                return null; // pretend TableSortRuleConvert is not specified (thus avoiding a stack overflow)
+            return base.ResolveContractConverter(objectType);
+        }
+    }
+
+    public class ActionConverter : JsonConverter
+    {
+        static readonly JsonSerializerSettings SpecifiedSubclassConversion = new JsonSerializerSettings() 
+        { 
+            ContractResolver = new BaseSpecifiedConcreteClassConverter() 
+        };
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(Action));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+            switch (jo["type"].Value<String>())
+            {
+                case "KEY":
+                    return JsonConvert.DeserializeObject<ActionKey>(jo.ToString(), SpecifiedSubclassConversion);
+                case "TYPING":
+                    return JsonConvert.DeserializeObject<ActionTyping>(jo.ToString(), SpecifiedSubclassConversion);
+                case "SOUND":
+                    return JsonConvert.DeserializeObject<ActionSound>(jo.ToString(), SpecifiedSubclassConversion);
+                default:
+                    throw new Exception();
+            }
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException(); // won't be called because CanWrite returns false
+        }
+    }
+
+    [JsonConverter(typeof(ActionConverter))]
+    [JsonObject(MemberSerialization.OptIn)]
     public abstract class Action
     {
+        [JsonProperty]
+        private String type;
+
         public long TimeScheduledMS
         {
             get => this.timeScheduledMS;
             set => this.timeScheduledMS=value;
         }
+        protected string Type
+        {
+            get => this.type;
+            set => this.type=value;
+        }
 
         private long timeScheduledMS;         // Time relative to triggering event
+
+        public abstract void Play();
+
+        // TODO split Action scheduling into Scheduler class 
+        public abstract Action[] Schedule();
     }
 }
