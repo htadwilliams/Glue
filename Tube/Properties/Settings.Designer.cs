38742aad0bba76662037633799981eee7bfbf913
Oedwak <Oedwak@comcast.net>
2019-08-18 14:48:32 -0700

    GUI work: * Add status bar with mouse coordinates, last clicked location, and mouse coordinate mode translation option. * Don't write example content over files that fail to load! Only create if file doesn't exist.

--- Tube/Properties/Settings.Designer.cs
+++ Tube/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace Glue.Properties {
                 this["ViewButtons"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool NormalizeMouseCoords {
+            get {
+                return ((bool)(this["NormalizeMouseCoords"]));
+            }
+            set {
+                this["NormalizeMouseCoords"] = value;
+            }
+        }
     }
 }
