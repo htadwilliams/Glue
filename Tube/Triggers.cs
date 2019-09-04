9e4479b31df73668ff774108a20cef4c592f6070
Oedwak <Oedwak@comcast.net>
2019-09-04 15:27:20 -0700

    Bug fix: Multiple triggers may be bound to the same key * Trigger Dictionary used to be <Keys, Trigger>, now is <Keys, List<Trigger>> * Added wrapper for Dictionary called Triggers for easier access. * Multiple triggers may fire on the same keystroke. Eating input is an OR operation between firing triggers, so if any trigger designates that the input must be eaten, it will be eaten. * Order of triggers is undefined when more than one is fired by the same keystroke. List order is affected by de/serialization, and would need a priority feature and sort them by priority or time. * Serialized json format shouldn't need to change with this fix.

---
+++ Tube/Triggers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Glue 
+{
+    class TriggerMap : Dictionary<Keys, List<Trigger>>
+    {
+        public TriggerMap(int initialCount) : base(initialCount)
+        {
+        }
+
+        public TriggerMap() : base()
+        {
+        }
+
+        public void Add(Trigger trigger)
+        {
+            if (!TryGetValue(trigger.TriggerKey, out List<Trigger> triggerList))
+            {
+                triggerList = new List<Trigger>();
+                Add(trigger.TriggerKey, triggerList);
+            }
+            triggerList.Add(trigger);
+        }
+    }
+}
